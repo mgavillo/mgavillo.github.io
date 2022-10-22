98f7421bbd4b6fd1ee470652a38691d98d634286
mgavillo <mgavillo@student.42.fr>
2022-10-23 00:10:46 +0100

    grow tree first anim

--- src/Home/05/Grow.tsx
+++ src/Home/05/Grow.tsx
@@ -1,11 +1,32 @@
-import React, { FunctionComponent }  from 'react';
+import React, { FunctionComponent, useEffect, useRef }  from 'react';
+import Svg from './Svg';
+import "./Grow.scss";
+const setProp = (ref:any, prop:any, value:any) => ref.current.style.setProperty(prop, value);
 
 export const Grow:FunctionComponent = () => {
-    return(
-        <div id="grow-wrapper" className='home-wrapper'>
-            <h2>And i'm always<br/>keen to grow</h2>
-            <div>
 
+    let paths;
+    const ref = useRef<HTMLDivElement | null>(null);
+    useEffect(() => {
+        window.addEventListener("scroll", function (e) {
+        
+            let top = ref?.current?.getBoundingClientRect().top
+            if(!top) return
+            top *= -1;
+
+            let scrollPercentage = top < 0 ? 0 :top / (window.innerHeight * 2); // Length to offset the dashes
+
+            let tree = document.getElementById("tree");
+            
+            console.log(scrollPercentage)
+            setProp(ref, "--scroll", `${-400 + (scrollPercentage * 600)}px`)
+        })
+    }, [])
+    return(
+        <div ref={ref} id="grow-wrapper" className='home-wrapper'>
+            <h2>And i'm always<br/>keen to grow</h2>
+            <div id="svg-wrapper">
+                <Svg/>
             </div>
         </div>
     )
---
+++ src/Home/05/Svg.tsx
@@ -0,0 +1,696 @@
+import * as React from "react"
+
+const Svg = () => (
+  <svg
+    id="tree"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 449 817"
+  >
+    <defs>
+      <linearGradient
+        id="linear-gradient"
+        x1={195.17}
+        y1={381.24}
+        x2={584.49}
+        y2={370.77}
+        gradientTransform="matrix(1 0 0 -1 -203.43 888.64)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} stopColor="#fff" />
+        <stop offset={0.43} stopColor="#8ca5ff" />
+        <stop offset={0.82} stopColor="#2757ff" />
+        <stop offset={1} stopColor="#0038ff" />
+      </linearGradient>
+      <linearGradient
+        id="linear-gradient-2"
+        x1={579.91}
+        y1={374.78}
+        x2={476.53}
+        y2={360.96}
+        gradientTransform="matrix(1 0 0 -1 -203.43 888.64)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} stopColor="#0ff" />
+        <stop offset={1} stopColor="#ff00e5" />
+      </linearGradient>
+      <linearGradient
+        id="linear-gradient-3"
+        x1={476.3}
+        y1={482.25}
+        x2={476.3}
+        y2={362.12}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-4"
+        x1={226.96}
+        y1={434.29}
+        x2={583.38}
+        y2={421.82}
+        xlinkHref="#linear-gradient"
+      />
+      <linearGradient
+        id="linear-gradient-5"
+        x1={579.91}
+        y1={426.67}
+        x2={476.53}
+        y2={412.85}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-6"
+        x1={476.3}
+        y1={534.14}
+        x2={476.3}
+        y2={414}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-7"
+        x1={136.65}
+        y1={505.09}
+        x2={587.35}
+        y2={484.78}
+        xlinkHref="#linear-gradient"
+      />
+      <linearGradient
+        id="linear-gradient-8"
+        x1={462.42}
+        y1={506.22}
+        x2={277.31}
+        y2={802.34}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-9"
+        x1={445.89}
+        y1={495.89}
+        x2={260.78}
+        y2={792.01}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-10"
+        x1={436.06}
+        y1={489.74}
+        x2={250.95}
+        y2={785.86}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-11"
+        x1={467.23}
+        y1={509.22}
+        x2={282.12}
+        y2={805.34}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-12"
+        x1={421.15}
+        y1={480.42}
+        x2={236.04}
+        y2={776.54}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-13"
+        x1={428}
+        y1={470.4}
+        x2={331.05}
+        y2={887.89}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-14"
+        x1={416.47}
+        y1={467.72}
+        x2={319.51}
+        y2={885.21}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-15"
+        x1={429.75}
+        y1={470.8}
+        x2={332.79}
+        y2={888.29}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-16"
+        x1={453.19}
+        y1={476.25}
+        x2={356.24}
+        y2={893.74}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-17"
+        x1={454.06}
+        y1={476.45}
+        x2={357.11}
+        y2={893.94}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-18"
+        x1={488.71}
+        y1={484.49}
+        x2={391.76}
+        y2={901.99}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-19"
+        x1={469.4}
+        y1={480.01}
+        x2={372.44}
+        y2={897.5}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-20"
+        x1={474.27}
+        y1={481.14}
+        x2={377.31}
+        y2={898.63}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-21"
+        x1={479.83}
+        y1={482.43}
+        x2={382.87}
+        y2={899.92}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-22"
+        x1={455.15}
+        y1={476.7}
+        x2={358.2}
+        y2={894.19}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-23"
+        x1={448.27}
+        y1={475.1}
+        x2={351.31}
+        y2={892.59}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-24"
+        x1={486.85}
+        y1={449.31}
+        x2={454.06}
+        y2={888.8}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-25"
+        x1={492.42}
+        y1={449.72}
+        x2={459.62}
+        y2={889.22}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-26"
+        x1={480.06}
+        y1={448.8}
+        x2={447.27}
+        y2={888.3}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-27"
+        x1={513.37}
+        y1={451.29}
+        x2={480.58}
+        y2={890.78}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-28"
+        x1={488.29}
+        y1={455.45}
+        x2={519.95}
+        y2={783.66}
+        gradientTransform="matrix(1 0 0 -1 -203.43 888.64)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} stopColor="#0ff" />
+        <stop offset={0.99} stopColor="#ff00e5" />
+      </linearGradient>
+      <linearGradient
+        id="linear-gradient-29"
+        x1={480.69}
+        y1={456.19}
+        x2={512.35}
+        y2={784.39}
+        xlinkHref="#linear-gradient-28"
+      />
+      <linearGradient
+        id="linear-gradient-30"
+        x1={477.71}
+        y1={456.48}
+        x2={509.37}
+        y2={784.68}
+        xlinkHref="#linear-gradient-28"
+      />
+      <linearGradient
+        id="linear-gradient-31"
+        x1={476.47}
+        y1={462.73}
+        x2={641.85}
+        y2={870.29}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-32"
+        x1={443.59}
+        y1={476.08}
+        x2={608.97}
+        y2={883.63}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-33"
+        x1={471.53}
+        y1={464.74}
+        x2={636.91}
+        y2={872.3}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-34"
+        x1={500.88}
+        y1={452.83}
+        x2={666.25}
+        y2={860.39}
+        xlinkHref="#linear-gradient-2"
+      />
+      <linearGradient
+        id="linear-gradient-35"
+        x1={461.33}
+        y1={468.88}
+        x2={626.71}
+        y2={876.43}
+        xlinkHref="#linear-gradient-2"
+      />
+      <style>
+        {".cls-2{fill:#0038ff}.cls-3{fill:#041d75}.cls-4{fill:#ff00e5}"}
+      </style>
+    </defs>
+    <path
+      className="cls-4"
+      d="M362.87 459.7C444.9 527.9 319.74 579 266.53 579s-143.46-38.15-96.34-119.3c0-81.57 43.13-147.7 96.34-147.7s96.34 66.13 96.34 147.7Z"
+      id="Ellipse_30"
+      style={{
+        opacity: 0.5,
+      }}
+    />
+    <g id="base">
+      <g id="Group">
+        <path
+          id="Vector"
+          className="cls-2"
+          d="M161.64 555.23v-40.27l111.23 64.22v40.27l-111.23-64.22Z"
+        />
+        <path
+          id="Vector_2"
+          className="cls-3"
+          d="m384.09 514.96-111.23 64.22v40.27l111.23-64.22v-40.27Z"
+        />
+        <path
+          id="Vector_3"
+          d="m272.86 579.18-111.23-64.22 111.23-64.21 111.23 64.21-111.23 64.22Z"
+          style={{
+            fill: "url(#linear-gradient)",
+          }}
+        />
+      </g>
+      <g id="Group_2">
+        <path
+          id="Vector_4"
+          className="cls-4"
+          d="M168.82 514.99v-48.54l104.04 60.07v48.54l-104.04-60.07Z"
+        />
+        <path
+          id="Vector_5"
+          d="m376.91 466.46-104.04 60.07v48.54L376.91 515v-48.54Z"
+          style={{
+            fill: "url(#linear-gradient-2)",
+          }}
+        />
+        <path
+          id="Vector_6"
+          d="m272.86 526.52-104.04-60.06 104.04-60.07 104.04 60.07-104.04 60.06Z"
+          style={{
+            fill: "url(#linear-gradient-3)",
+          }}
+        />
+      </g>
+      <g id="Group_3">
+        <path
+          id="Vector_7"
+          className="cls-2"
+          d="M161.64 503.34v-40.27l111.23 64.22v40.27l-111.23-64.21Z"
+        />
+        <path
+          id="Vector_8"
+          className="cls-3"
+          d="m384.09 463.07-111.23 64.22v40.27l111.23-64.21v-40.27Z"
+        />
+        <path
+          id="Vector_9"
+          d="m272.86 527.29-111.23-64.22 111.23-64.21 111.23 64.21-111.23 64.22Z"
+          style={{
+            fill: "url(#linear-gradient-4)",
+          }}
+        />
+      </g>
+      <g id="Group_4">
+        <path
+          id="Vector_10"
+          className="cls-4"
+          d="M168.82 463.1v-48.54l104.04 60.07v48.54L168.82 463.1Z"
+        />
+        <path
+          id="Vector_11"
+          d="m376.91 414.56-104.04 60.07v48.54l104.04-60.07v-48.54Z"
+          style={{
+            fill: "url(#linear-gradient-5)",
+          }}
+        />
+        <path
+          id="Vector_12"
+          d="m272.86 474.63-104.04-60.07 104.04-60.07 104.04 60.07-104.04 60.07Z"
+          style={{
+            fill: "url(#linear-gradient-6)",
+          }}
+        />
+      </g>
+      <g id="Group_5">
+        <path
+          id="Vector_13"
+          className="cls-2"
+          d="M161.64 450.74v-51.89l111.23 64.21v51.89l-111.23-64.21Z"
+        />
+        <path
+          id="Vector_14"
+          className="cls-3"
+          d="m384.09 398.85-111.23 64.21v51.89l111.23-64.21v-51.89Z"
+        />
+        <path
+          id="Vector_15"
+          d="m272.86 463.07-111.23-64.21 111.23-64.22 111.23 64.22-111.23 64.21Z"
+          style={{
+            fill: "url(#linear-gradient-7)",
+          }}
+        />
+      </g>
+    </g>
+    <g id="root">
+      <path
+        id="_1"
+        d="M201.5 508.5V576L1 694v123"
+        style={{
+          stroke: "#0ff",
+          fill: "none",
+          strokeWidth: 2,
+        }}
+      />
+    </g>
+    <g id="branches">
+      <g id="branch_5">
+        <path
+          id="Line_143"
+          d="m99 167.14 8.87 8.87 12.57 12.57 6.34 6.34v28.63l10.3 10.3 10.3 10.3 31.9 31.9h35.18l35.5 35.5v67.75l-23.13-12.63v21.15"
+          style={{
+            fill: "none",
+            strokeWidth: 2,
+            stroke: "url(#linear-gradient-8)",
+          }}
+        />
+        <path
+          id="Line_150"
+          d="M107.87 176.01v-12.36l-6.23-6.23H89.39l-7.08-7.08v-10.25"
+          style={{
+            stroke: "url(#linear-gradient-9)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_149"
+          d="m120.44 188.59-9.19 9.19H98.99l-28-28"
+          style={{
+            stroke: "url(#linear-gradient-10)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_148"
+          d="M147.38 244.15v-49.23l-22.29-22.29m-.21.07v-13.52l12.89-12.89v-19.44"
+          style={{
+            stroke: "url(#linear-gradient-11)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_147"
+          d="M137.08 233.85H84.73l-10.3-10.3"
+          style={{
+            stroke: "url(#linear-gradient-12)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_146"
+          d="M205.6 276.34c-4.33-4.09-12.93-12.34-12.68-12.68.25-.34.11-9.51 0-14.05l-7.29-7.29-20.39-20.39v-40.67"
+          style={{
+            stroke: "url(#linear-gradient-13)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_152"
+          d="m165.24 191.4-14.05-14.05v-15.21"
+          style={{
+            stroke: "url(#linear-gradient-14)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_151"
+          d="M185.63 242.32v-16.48l-10.46-10.46"
+          style={{
+            stroke: "url(#linear-gradient-15)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+      </g>
+      <g id="branch_4">
+        <path
+          id="Line_144"
+          d="M130.38 75.3v11.2l9.61 9.61 9.61 9.61v21.13l43.95 43.95 6.55 6.55v32.85l21.82 21.82 10.91 10.91 10.91 10.91 16.8 16.8-.85 121.63-18.93 11.32v10.08"
+          style={{
+            stroke: "url(#linear-gradient-16)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_153"
+          d="M171.69 71.81V54.27l-10.88-10.88v-9.61"
+          style={{
+            stroke: "url(#linear-gradient-17)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_152_2"
+          d="M225.46 152.53v-40.15L205.6 92.52V54.28L192.92 41.6"
+          style={{
+            stroke: "url(#linear-gradient-18)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_151_2"
+          d="M243.74 253.84V170.8l-18.28-18.28-50.29-50.29V75.29l-3.49-3.49-22.08-22.08V23.74"
+          style={{
+            stroke: "url(#linear-gradient-19)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_150_2"
+          d="M232.83 242.93v-24.06l-3.49-3.49-11.62-11.62v-18.07"
+          style={{
+            stroke: "url(#linear-gradient-20)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_149_2"
+          d="M229.35 215.39v-34.12l-13.52-13.52v-12.04"
+          style={{
+            stroke: "url(#linear-gradient-21)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_148_2"
+          d="M193.56 170.8v-29.05l-11.94-11.94v-6.76"
+          style={{
+            stroke: "url(#linear-gradient-22)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_147_2"
+          d="m200.11 193.78-16.43-16.43"
+          style={{
+            stroke: "url(#linear-gradient-23)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+      </g>
+      <g id="branch_3">
+        <path
+          id="Line_145"
+          d="M268.77 79.23v327.05l18.83 11.78v10.22"
+          style={{
+            stroke: "url(#linear-gradient-24)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_148_3"
+          d="m245.63 73 14.9-14.9V25.99l11.41-11.41V0"
+          style={{
+            stroke: "url(#linear-gradient-25)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_147_3"
+          d="m268.77 122.87-23.14-23.14V46.27l-15-15V4.97"
+          style={{
+            stroke: "url(#linear-gradient-26)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_146_2"
+          d="m268.77 156.57 33.7-33.7V70.79"
+          style={{
+            stroke: "url(#linear-gradient-27)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+      </g>
+      <g id="branch_2">
+        <path
+          id="Line_146_3"
+          d="M327.08 171.43v26.94l-12.26 12.25-12.25 12.26v18.17l-24.62 24.62v126.6l11.48-6.71 20.87 12.92v10.36"
+          style={{
+            stroke: "url(#linear-gradient-28)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_148_4"
+          d="m314.83 210.62-27.04-27.04v-24.19"
+          style={{
+            stroke: "url(#linear-gradient-29)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_147_4"
+          d="m302.58 222.88-14.79-14.79"
+          style={{
+            stroke: "url(#linear-gradient-30)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+      </g>
+      <g id="branch_1">
+        <path
+          id="Line_147_5"
+          d="m432.31 116.33-12.94 12.94-12.94 12.94v18.07l-29.26 29.26h-8.82l-18.96 18.96v9.46l-31.69 31.69v28l-30.22 30.21v69.88l14.47-9.51v18.31"
+          style={{
+            stroke: "url(#linear-gradient-31)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_151_3"
+          d="M375.1 142.21v-31.32l-21.5-21.5V70.8"
+          style={{
+            stroke: "url(#linear-gradient-32)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_150_3"
+          d="M368.34 189.54v-40.57l6.76-6.76 31.32-31.32V73.97"
+          style={{
+            stroke: "url(#linear-gradient-33)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_149_3"
+          d="M419.37 129.27v-24.35l10.3-10.3H449"
+          style={{
+            stroke: "url(#linear-gradient-34)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+        <path
+          id="Line_148_5"
+          d="M349.38 180.14v28.37"
+          style={{
+            stroke: "url(#linear-gradient-35)",
+            fill: "none",
+            strokeWidth: 2,
+          }}
+        />
+      </g>
+    </g>
+  </svg>
+)
+
+export default Svg
